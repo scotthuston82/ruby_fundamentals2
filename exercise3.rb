d401afef8edcfb958839b688e14527d10a1c580f
Scott <scotthuston1982@gmail.com>
2017-12-12 14:39:07 -0500

    exercise 4 finished

---
+++ exercise3.rb
@@ -0,0 +1,9 @@
+def iseven(number)
+  awnser = number % 2
+  if awnser == 0
+    return true
+  else
+    return false
+  end
+end
+puts iseven(4)
---
+++ exercise4.rb
@@ -0,0 +1,4 @@
+def length(string)
+  string.length < 8
+end
+puts length("scotthuston")
